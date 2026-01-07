da44db3f68672e9e48f4e50e43b5c16be617f618
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-01-07 20:13:34 +0530

    add database connection file into the codebase

--- db/index.js
+++ db/index.js
@@ -1,5 +1,18 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
 
-const connectDB = async () => {};
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `Database you're connected to is ${connectionInstance.connection.name} and the connection is in state ${connectionInstance.connection.readyState}`
+    );
+  } catch (error) {
+    console.log(`ERROR Connecting to MongoDB`, error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
