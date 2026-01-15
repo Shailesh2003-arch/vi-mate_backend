04aff0d11088a1ee0a1f82bd9c7613f2b4df1ed0
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-01-15 14:45:26 +0530

    chore(app.js): import and declare app routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import ApiError from "./utils/ApiError";
+import ApiError from "./utils/ApiError.js";
 
 const app = express();
 
@@ -19,6 +19,13 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// importing router...
+
+import userRouter from "./routes/user.routes.js";
+
+// declaring routes...
+app.use("/api/v1/users", userRouter);
 
 app.use((err, req, res, next) => {
   if (err instanceof ApiError) {
