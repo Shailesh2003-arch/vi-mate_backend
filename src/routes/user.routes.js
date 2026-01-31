f83730809519b39582512679d81a8c01586c2ef9
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-01-31 10:08:50 +0530

    add change-password, credential, avatar and cover-image routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,14 +4,28 @@ import {
   registerUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  updateUserDetails,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
-
+import {upload} from "../middlewares/multer.js";
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/change-credential").post(verifyJWT, updateUserDetails);
+router
+  .route("/update-user-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router.route(
+  "/update-cover-image",
+  upload.single("coverImage"),
+  updateCoverImage
+);
 
 export default router;
