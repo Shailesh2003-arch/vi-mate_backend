17488a009ca439c2e348850d65ca9adeacb2518c
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-01-31 10:04:45 +0530

    chore (constants): add default avatar and cover-images

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
 export const DB_NAME = "vi-mate";
+export const DEFAULT_AVATAR_URL = "https://res.cloudinary.com/dv2miz7hz/image/upload/v1767777012/default-avatar.png_cxobtu.png";
+export const DEFAULT_COVER_IMAGE_URL = "https://res.cloudinary.com/dv2miz7hz/image/upload/v1767775630/samples/landscapes/landscape-panorama.jpg";
