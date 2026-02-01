bd3b0e9302f4792748ce6c40df3c12b9404c6ef4
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-02-01 15:43:36 +0530

    chore (video-routes) : add video publishing route

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,8 +1,22 @@
 import {Router} from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
+import {publishVideo} from "../controllers/video.controller.js";
+import {upload} from "../middlewares/multer.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/publish").post();
+router.route("/publish").post(
+  upload.fields([
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+    {
+      name: "videoFile",
+      maxCount: 1,
+    },
+  ]),
+  publishVideo
+);
 export default router;
