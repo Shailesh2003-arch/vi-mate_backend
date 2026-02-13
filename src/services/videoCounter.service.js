d36c36a0809cebb26eccac87babc4683ff624241
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-02-13 20:02:20 +0530

    feat (video-counter-service) : add video-views-counter and video-views-deduplication feature

--- src/services/videoCounter.service.js
+++ src/services/videoCounter.service.js
@@ -1,7 +1,12 @@
 import redis from "../config/redis.js";
-import { videoViewsKey } from "../utils/redis/rediskeys.js";
+import {videoCounterKey, videoViewsKey} from "../utils/redis/rediskeys.js";
 
-export const incrementVideoView = async(videoId)=>{
-    const key = videoViewsKey(videoId);
-    await redis.incr(key);
-}
+export const incrementVideoView = async (videoId, userId) => {
+  const dedupeKey = videoViewsKey(videoId, userId);
+  // this checks if the user has watched the video in last 24 hrs, if not it sets the key which expires after 24 hrs
+  // and then we increment the view counter.
+  const result = await redis.set(dedupeKey, "1", "NX", "EX", 86400);
+  if (result) {
+    await redis.incr(videoCounterKey(videoId));
+  }
+};
