f1d0df65ae649a82fbb5f9c34df9994b878815e6
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-02-14 19:48:41 +0530

    chore (redisKeys) : add dirty-videos-set, likes, dislikes, and user-reaction keys

--- src/utils/redis/redisKeys.js
+++ src/utils/redis/redisKeys.js
@@ -1,2 +1,13 @@
-export const videoViewsKey = (videoId,userId) =>`video:view:${videoId}:${userId}`;
-export const videoCounterKey = videoId =>`video:${videoId}:views`
+export const videoViewsKey = (videoId, userId) =>
+  `video:view:${videoId}:${userId}`;
+export const videoCounterKey = (videoId) => `video:${videoId}:views`;
+
+// user-reaction state...
+export const userReactionKey = (videoId, userId) =>
+  `reaction:${videoId}:${userId}`;
+
+// like counter
+export const likesDeltaKey = (videoId) => `video:${videoId}:likes`;
+// dislike counter
+export const dislikesDeltaKey = (videoId) => `video:${videoId}:dislikes`;
+export const dirtyVideosKey = () => `videos:dirty`;
