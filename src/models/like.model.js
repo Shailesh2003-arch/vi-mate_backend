e31abb8212bc600d688b60284d0d28e56c823188
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-06-26 10:34:26 +0530

    chore (like-mode) : add like model schema

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["like", "dislike"],
+      required: true,
+    },
+  },
+  {timestamps: true}
+);
+
+likeSchema.index({user: 1, video: 1}, {unique: true});
+export const Like = mongoose.model("Like", likeSchema);
