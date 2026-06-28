84dd73c540bd11c2347fe14155dfdfb441b7a7de
Shailesh2003-arch <thoratshailesh220@gmail.com>
2026-06-28 14:09:52 +0530

    feat (subscription-route) : add toggle-subscription route

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,8 +1,12 @@
 import {Router} from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
-import getMySubscriptions from "../controllers/subscription.controller.js";
+import {
+  getMySubscriptions,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
 const router = Router();
 
 router.route("/").get(verifyJWT, getMySubscriptions);
+router.route("/:channelId").post(verifyJWT, toggleSubscription);
 
 export default router;
